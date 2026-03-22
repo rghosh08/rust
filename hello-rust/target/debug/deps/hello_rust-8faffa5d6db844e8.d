a8cea2ff0a88869630303e7b39af3d950f19c55c
rghosh08 <rajat.ghosh@nutanix.com>
2026-03-22 13:30:24 -0700

    hello world

---
+++ hello-rust/target/debug/deps/hello_rust-8faffa5d6db844e8.d
@@ -0,0 +1,5 @@
+/Users/rajat/Documents/CODE/rust/hello-rust/target/debug/deps/hello_rust-8faffa5d6db844e8.d: src/main.rs
+
+/Users/rajat/Documents/CODE/rust/hello-rust/target/debug/deps/hello_rust-8faffa5d6db844e8: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/rajat/Documents/CODE/rust/hello-rust/target/debug/hello-rust: /Users/rajat/Documents/CODE/rust/hello-rust/src/main.rs
